daa9240d476aec54bc0585456690b584b36a9ea0
Chiquadro841 <152106838+Chiquadro841@users.noreply.github.com>
2024-08-14 21:28:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,12 +77,11 @@ st.write("Benvenuto nell'app di previsione degli esiti degli incontri. Seleziona
          "l'esito dell'incontro.")
 
 # Testo per la forza di squadra con ritorni a capo
-st.text(
-    "Per la forza di squadra, si usa come base già K:                                                               \n"
-    "es forza 5000K --> 5000                                                                                         \n"
-    "50M --> 50000                                                                                                  \n"
-    "50B --> 50000000"
-)
+st.text("Per la forza di squadra, si usa come base già K:")
+st.text("es forza 5000K --> 5000")                                                                                       
+st.text("50M --> 50000")                                                                                                 
+st.text("50B --> 50000000")
+
 # Selezione e livello degli eroi per le posizioni
 inputs = {col: 0 for col in feature_columns}
 
